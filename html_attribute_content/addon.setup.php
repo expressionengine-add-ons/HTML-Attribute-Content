22d949a893948944fba12864116977e212e8262c
Derek Jones <derek.jones@ellislab.com>
2017-02-14 06:30:39 -0800

    removing spaces from release file at Travi CI's suggestion

--- html_attribute_content/addon.setup.php
+++ html_attribute_content/addon.setup.php
@@ -5,7 +5,7 @@ return array(
 	'author_url'  => 'https://ellislab.com/',
 	'name'        => 'HTML Attribute Content',
 	'description' => 'Preps content for use inside an HTML tag attribute',
-	'version'     => '2.1.3',
+	'version'     => '2.1.4',
 	'namespace'   => 'EllisLabOS\HTMLAttributeContent',
 	'settings_exist' => FALSE
 );
